d147562de27bc6e5da8fbd1f7c1d0b0fb9ed7038
angelfdezcivila <angelfdezcivila@uma.es>
2022-11-28 03:34:37 +0100

    editHouse added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Houses from '../pages/listHouses';
 import CreateHouse from '../pages/createHouse';
 import Home from '../pages/home';
-// import EditHouse from '../pages/editHouse';
+import EditHouse from '../pages/editHouse';
 
 function AppRouter() {
   return (
@@ -14,8 +14,8 @@ function AppRouter() {
           element={<CreateHouse />} />
         <Route path='/home'
           element={<Home />} />
-        {/* <Route path="viviendas/propietario/:id/vivienda/:idVivienda/edit"
-              element={<EditHouse/>}/> */}
+        <Route path="viviendas/propietario/:id/vivienda/:idVivienda/edit"
+              element={<EditHouse/>}/>
       </Routes>
     </BrowserRouter>
 
