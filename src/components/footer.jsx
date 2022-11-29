3b6be19d42a458076f54dc1c9972956ec834f68f
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-11-29 04:11:50 +0100

    refactor: fix header and footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 export const Footer = () => {
 
+  const backgroud = {
+    background: '#f5f5f5'
+  }
+
   return (
 
-    <footer className="fixed-bottom py-1 d-flex flex-wrap justify-content-between align-items-center border-top">
+    <footer className="fixed-bottom bg-light py-0 px-4 d-flex flex-wrap justify-content-between align-items-center border-top"
+      style={backgroud}>
       <p className="col-md-4 ms-4 mb-0 text-muted">© 2022 SwishHouse, Inc</p>
 
       <ul className="nav col-md-4 me-4 justify-content-end">
