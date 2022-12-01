aa1eacb30d30510bcd1a01c6eaa23d03b8eb5197
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-12-01 14:14:33 +0100

    Improve view of images #5

--- src/helper/SetUpHouse.js
+++ src/helper/SetUpHouse.js
@@ -5,10 +5,12 @@ const baseUrl = Global.baseUrl;
 export const setUpHouse = ( id, setHouse ) => {
   
   fetch( `${baseUrl}viviendas/${id}` )
-    .then( res => res.json())
-    .then( data => {
-      console.log(data)
-      setHouse( data );
-    })
+    .then(res => {
+        if (res.status === 200) {
+            res.json().then(data => {
+                setHouse(data);
+            });
+        };
+    });
     
 };
