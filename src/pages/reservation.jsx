7cdf62a6278fee653abd58d09d647fd982b94b62
AngeloGC <0619974249@uma.es>
2022-12-02 23:11:14 +0100

    Update reservation.jsx

--- src/pages/reservation.jsx
+++ src/pages/reservation.jsx
@@ -49,6 +49,36 @@ const Reservation = () => {
                         </button>
                     </div>
 
+                    <table class="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
                     <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
                         <div className="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
