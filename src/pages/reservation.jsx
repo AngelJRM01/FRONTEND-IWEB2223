55cf9e0629205f62d9a4242eb28554a1d25d2946
AngeloGC <0619974249@uma.es>
2022-12-03 03:01:12 +0100

    Finish first container

--- src/pages/reservation.jsx
+++ src/pages/reservation.jsx
@@ -22,42 +22,60 @@ const Reservation = () => {
 
                 <div className="col data">
 
-                    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-                        <div className="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <div className='firstContainer'>
+
+                        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+                            <div className="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="https://top-mmo.fr/wp-content/uploads/2022/11/l-intro-1667494654.jpg" className="d-block w-100" alt="..."/>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="..." className="d-block w-100" alt="..."/>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="..." className="d-block w-100" alt="..."/>
+                                </div>
+                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
                         </div>
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src="https://a0.muscache.com/im/pictures/43a44c04-8eb5-4cd3-a930-397d7276d03c.jpg" className="d-block w-100" alt="..."/>
+
+                        <div className='flexito1'>
+                            <div className='fl'>
+                                <p className='title'>Llegada:</p>
+                                <p className='date'>&ensp;mié, 11 ene.</p>
+                                <p className='time'>&ensp;14:00</p>
                             </div>
-                            <div className="carousel-item">
-                                <img src="..." className="d-block w-100" alt="..."/>
-                            </div>
-                            <div className="carousel-item">
-                                <img src="..." className="d-block w-100" alt="..."/>
+                            <div className='fl salida'>
+                                <p className='title'>Salida:</p>
+                                <p className='date'>&emsp;jue, 12 ene.</p>
+                                <p className='time'>&emsp;11:00</p>
                             </div>
                         </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
+
+                        <div className="list-group bottonss">
+                            <a href="http://localhost:3000/vivienda/6371a2d0cbdef810a6e09c83" className="list-group-item list-group-item-action house">
+                                <i class="fa-solid fa-pager">
+                                </i>&emsp;Mostrar anuncio
+                                <i class="fa-sharp fa-solid fa-chevron-right arrow"></i>
+                            </a>
+                        </div>
                     </div>
 
-                    <table class="table">
-                            <thead>
-                                <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                                </tr>
-                            </thead>
+                    <div className='secondContainer'>
+                        <div className='auxDiv'></div>
+                        <table className="table">
                             <tbody>
                                 <tr>
                                 <th scope="row">1</th>
@@ -78,32 +96,7 @@ const Reservation = () => {
                                 </tr>
                             </tbody>
                         </table>
-
-                    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-                        <div className="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src="https://a0.muscache.com/im/pictures/43a44c04-8eb5-4cd3-a930-397d7276d03c.jpg" className="d-block w-100" alt="..."/>
-                            </div>
-                            <div className="carousel-item">
-                                <img src="..." className="d-block w-100" alt="..."/>
-                            </div>
-                            <div className="carousel-item">
-                                <img src="..." className="d-block w-100" alt="..."/>
-                            </div>
-                        </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
+                        <div className='auxDiv'></div>
                     </div>
 
                     <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
