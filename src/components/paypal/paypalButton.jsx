b8cd5885827df193791e15ff77eaec03142f1da9
Galo <galopg2001@hotmail.com>
2022-12-26 00:15:25 +0100

    Add paypal button

---
+++ src/components/paypal/paypalButton.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+const PaypalButton = ({precio}) => {
+    const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+    const createOrder = (data, actions) => {
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: precio
+            },
+          },
+        ],
+      });
+    };
+    const onApprove = (data, actions) => {
+      return actions.order.capture();
+    };
+    return (
+      <div>
+        <PayPalButton
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+        /> 
+      </div> 
+    );
+}  
+
+export default PaypalButton;
+
+/* export function PaypalButton({precio}) {
+   console.log("hola" + precio);
+    const createOrder = (data, actions) => {
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: precio,
+            },
+          },
+        ],
+      });
+    };
+    const onApprove = (data, actions) => {
+      return actions.order.capture();
+    };
+    return (
+  
+      <PayPalButton
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+      />
+    );
+  } */
