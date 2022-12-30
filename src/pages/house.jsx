fb6355a89dbc285f12caa0cf5c32ff213a860f58
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-12-30 19:58:42 +0100

    Fix: create comment

--- src/pages/house.jsx
+++ src/pages/house.jsx
@@ -19,7 +19,7 @@ import { setUpGasStation } from "../helper/SetUpGasStation";
 import { setUpTourist } from "../helper/setUpTourist";
 import ModalConfirmationReservation from "../components/house/modalConfirmationReservation";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Global } from '../../helper/Global';
+import { Global } from '../helper/Global';
 
 const House = () => {
 
@@ -37,7 +37,7 @@ const House = () => {
     const { user, isAuthenticated } = useAuth0();
     const [valueCapacity, setValueCapacity] = useState({value: 1, label: 1});
     const baseUrl = Global.baseUrl
-    let URIVivienda = ''
+    const URIVivienda = `${baseUrl}viviendas/`+id
 
     useEffect( () => {
 
@@ -52,7 +52,6 @@ const House = () => {
             setUpReservationsOfAHouse( house._id, setReservations );
             setUpGasStation(house.coordenadas.latitud, house.coordenadas.longitud, 20, setGasStation)
             setUpTourist(house.coordenadas.latitud, house.coordenadas.longitud, setTourist)
-            URIVivienda = `${baseUrl}viviendas/`+house._id
         }
 
     }, [house])
@@ -100,7 +99,7 @@ const House = () => {
     const handleSubmit = () => {
         let comentario = {
             vivienda: house._id,
-            usuario: user.email,
+            usuario: user.name,
             likes: [],
             dislikes: [],
             mensaje: comment,
@@ -284,14 +283,16 @@ const House = () => {
                             }
                             
                             {isAuthenticated ?
-                                <form className="mt-4" onSubmit={handleSubmit}>
-                                    <label>Añade un comentario</label>
-                                    <input  type="text" 
-                                            className="form-control mt-1"
-                                            value={comment}
-                                            onChange={e => setComment(e.target.value)}></input>
-                                    <button className="btn btn-primary mt-3" type="submit">Comentar</button>
-                                </form>
+                                <div>
+                                    <form className="mt-4" onSubmit={handleSubmit}>
+                                        <label>Añade un comentario</label>
+                                        <input  type="text" 
+                                                className="form-control mt-1"
+                                                value={comment}
+                                                onChange={e => setComment(e.target.value)}></input>
+                                        <button className="btn btn-primary mt-3" type="submit">Comentar</button>
+                                    </form>
+                                </div>
                                 :
                                 <div></div>
                             }
