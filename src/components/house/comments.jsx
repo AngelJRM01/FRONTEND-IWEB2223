c723cc15995b95ae65c287bad63259c8d7a7b476
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-12-31 14:17:59 +0100

    fix: comment break

--- src/components/house/comments.jsx
+++ src/components/house/comments.jsx
@@ -104,7 +104,7 @@ const Comments = ({comentario, user, house, comentarios}) => {
     return (
         <div>
             <p><img className="imagenComments" alt={`Imagen de ${comentario.usuario}`} src={comentario.imagenUsuario}/> <b className='mx-2'>{comentario.usuario}</b> <small>{fechaComentario()}</small></p>
-            <div className="row px-5"><p className='breakSpaces'>{comentario.mensaje}</p></div>
+            <div className="row px-5"><p className='breakSpaces text-break'>{comentario.mensaje}</p></div>
             <button onClick={votarLike} disabled={comentario.likes.includes(user.name)} className={comentario.likes.includes(user.name) ? 'votado' : 'like'}><i className="fa-solid fa-thumbs-up"></i> {comentario.likes.length}</button>
             <button onClick={votarDislike} disabled={comentario.dislikes.includes(user.name)} className={comentario.dislikes.includes(user.name) ? 'votado mx-2' : 'dislike mx-2'}><i className="fa-sharp fa-solid fa-thumbs-down"></i> {comentario.dislikes.length}</button>
             <hr/>
